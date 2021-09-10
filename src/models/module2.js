05d251dd9ceb6a89c1ff88cb555b9e00dd7389f8
pshah1102 <shahpoojan@protonmail.com>
2021-09-10 10:36:55 +0530

    Added apis for module2

---
+++ src/models/module2.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const questionschema=new mongoose.Schema({
+    question:
+    {type:String
+   },
+
+    options:[String],
+   answer:{
+       type:Number
+   },
+   image:{
+       type:String
+   }
+});
+
+const module2= new mongoose.model("module2",questionschema)
+module.exports=module2;
