a0fec49457bae8edf4a36b70b37626ea1db69d03
pshah1102 <shahpoojan@protonmail.com>
2021-11-10 14:35:50 +0530

    cors error solved

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 5000;
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://lucid-bartik-7a57a1.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
