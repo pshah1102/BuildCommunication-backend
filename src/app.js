fd2ccc7c233622e02d0e61434d9e3fcdc66e5aa8
DevPatel210 <devhpatel2@gmail.com>
2021-11-10 15:04:22 +0530

    Solved bugs related to cookies

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(function (req, res, next) {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://buildcommunication.netlify.app,http://localhost:3000"
+    "https://buildcommunication.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
