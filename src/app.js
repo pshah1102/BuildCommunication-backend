1cc6a424417074dc483846e27abffdf5d9176407
pshah1102 <shahpoojan@protonmail.com>
2021-11-10 14:18:05 +0530

    Temp cors error solved

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 5000;
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
